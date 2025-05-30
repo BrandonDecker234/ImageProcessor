2222ca6d6be81b7d52954fc0f49af382fd9ba9f0
BrandonDecker234 <82422586+BrandonDecker234@users.noreply.github.com>
2025-05-30 12:07:11 -0400

    6 add image analysis endpoint (#11)
    * Added new test image
    
    * Added image controller analysis.
    
    * Added unit tests for ImageAnalysisController
    
    * Shows PingController.cs now
    
    ---------
    
    Co-authored-by: bdecker-navi <bdecker@navistone.com>

---
+++ ImageProcessor/ImageProcessor/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageProcessor.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PingController : ControllerBase
+{
+    // GET
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
---
+++ ImageProcessor/ImageProcessor/Middleware/ExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ImageProcessor.Middleware;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var statusCode = context.Exception switch
+        {
+            ValidationException => StatusCodes.Status400BadRequest,
+            UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+            ApplicationException => StatusCodes.Status500InternalServerError,
+            NullReferenceException => StatusCodes.Status404NotFound,
+            ArgumentException => StatusCodes.Status400BadRequest,
+            InvalidOperationException => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        context.Result = new ObjectResult(new
+        {
+            error = "From filter: \n\n" + context.Exception.Message,
+            stackTrace = context.Exception.StackTrace
+        })
+        {
+            StatusCode = statusCode
+        };
+    }
+}
